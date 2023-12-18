eef85397b1f884610b76d8588f0dbb58527ae3bc
Léo <117282572+CLipet-Leo@users.noreply.github.com>
2023-12-18 16:47:45 +0100

    Lockpad functionnal (no script linked yet)

--- src/Cadena.jsx
+++ src/Cadena.jsx
@@ -12,16 +12,33 @@ export function Cadena() {
     const handleClose = () => setShow(false);
     const handleShow = () => setShow(true);
 
-    const [validated, setValidated] = useState(false);
-
     const handleSubmit = (event) => {
         const form = event.currentTarget;
-        if (form.checkValidity() === false) {
-        event.preventDefault();
-        event.stopPropagation();
+
+        let firstCodeNumber = document.getElementById("firstCodeNumber").value;
+        let secondCodeNumber = document.getElementById("secondCodeNumber").value;
+        let thirdCodeNumber = document.getElementById("thirdCodeNumber").value;
+        let fourthCodeNumber = document.getElementById("fourthCodeNumber").value;
+
+        if (!firstCodeNumber) {firstCodeNumber =  0}
+        if (!secondCodeNumber) {secondCodeNumber = 0}
+        if (!thirdCodeNumber) {thirdCodeNumber = 0}
+        if (!fourthCodeNumber) {fourthCodeNumber = 0}
+
+        console.log(firstCodeNumber, secondCodeNumber, thirdCodeNumber, fourthCodeNumber);
+
+        if (firstCodeNumber === "1" && secondCodeNumber === "1" && thirdCodeNumber === "1" && fourthCodeNumber === "1") {
+            console.log("ouvert");
+            handleClose();
+        }
+        else {
+            console.log("fermé");
+            document.getElementById("locked").innerHTML = "Rien ne se passe..."; 
         }
 
-        setValidated(true);
+        form.checkValidity()
+        event.preventDefault();
+        event.stopPropagation();
     };
 
     
@@ -32,18 +49,19 @@ export function Cadena() {
         </Button>
     
         <Modal className='text-center' show={show} onHide={handleClose} centered>
-            <Form noValidate validated={validated} onSubmit={handleSubmit}>
+            <Form onSubmit={handleSubmit}>
                 <Modal.Header closeButton>
                     <Modal.Title className="ms-auto">Cadenas</Modal.Title>
                 </Modal.Header>
                 <Modal.Body>
-                    <Form.Group controlId="validationCustomUsername">
+                    <Form.Group>
                         <InputGroup className="mb-3" >
-                            <Form.Control className='text-center' placeholder="*" required />
-                            <Form.Control className='text-center' placeholder="*" required />
-                            <Form.Control className='text-center' placeholder="*" required />
-                            <Form.Control className='text-center' placeholder="*" required />
+                            <Form.Control className='text-center' id='firstCodeNumber' maxLength={1} pattern='\d{1,1}' name='codeNumber' placeholder="*" required />
+                            <Form.Control className='text-center' id='secondCodeNumber' maxLength={1} pattern='\d{1,1}' name='codeNumber' placeholder="*" required />
+                            <Form.Control className='text-center' id='thirdCodeNumber' maxLength={1} pattern='\d{1,1}' name='codeNumber' placeholder="*" required />
+                            <Form.Control className='text-center' id='fourthCodeNumber' maxLength={1} pattern='\d{1,1}' name='codeNumber' placeholder="*" required />
                         </InputGroup>
+                        <div id='locked'></div>
                     </Form.Group>
                 </Modal.Body>
                 <Modal.Footer style={{display: "flex", justifyContent: "center"}}>
